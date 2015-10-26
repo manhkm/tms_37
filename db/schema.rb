edf4ffc5132d8892d5f94702e9cab20763c4e718
Dinh Tung <dinh.tung@framgia.com>
2015-10-26 15:48:20 +0700

    user modelling, create user and session controller added

--- db/schema.rb
+++ db/schema.rb
@@ -85,10 +85,10 @@ ActiveRecord::Schema.define(version: 20151027010931) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
-    t.boolean  "supervision"
+    t.boolean  "supervisor"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
