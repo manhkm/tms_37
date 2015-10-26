38e060d2e0ba153f8774136d6b7ee458ebdb2ff7
manhkm <qmanh.kct@gmail.com>
2015-10-26 16:15:23 +0700

    staticpage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title = '')                     # Method def, optional arg
+
+    if page_title.empty?                              # Boolean test
+      t :title                                       # Implicit return
+    else
+      page_title + " | " + t(:title)                  # String concatenation
+    end
+  end
 end
+
--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BaseApp
+module Base
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -22,5 +31,9 @@ module BaseApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
