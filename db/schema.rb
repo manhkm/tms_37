7b74d8daf7e96d4815a7f777dd921f9d6597b72d
Dinh Tung <dinh.tung@framgia.com>
2015-10-27 13:16:36 +0700

    Add login logout function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027010931) do
+ActiveRecord::Schema.define(version: 20151027044244) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20151027010931) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
